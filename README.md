dc0a649b8801992892050df46cb01bc3e72bd48f
Rohit Kumar <rohit0kumar@hotmail.com>
2015-11-07 13:26:28 +0530

    add

---
+++ README.md
@@ -0,0 +1,3 @@
+
+# Simple location locator by pincode
+follow for demo: http://www.iamrohit.in/simple-suto-suggest-example-using-php-jquery-and-mysql/
---
+++ request.php
@@ -0,0 +1,31 @@
+<?php
+// Remove blow comments from header If  you are making calls from another server
+/*
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
+*/
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+$hostname = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "pincodes";
+$q = $_GET['q'];
+if(isset($q) || !empty($q)) {
+	$con = mysqli_connect($hostname, $username, $password, $dbname);
+    $query = "SELECT * FROM pincodes WHERE pincode LIKE '$q%'";
+    $result = mysqli_query($con, $query);
+    $res = array();
+    while($resultSet = mysqli_fetch_assoc($result)) {
+	 $res[$resultSet['id']]['id'] =  $resultSet['divisionname']."-".$resultSet['egionname']."-".$resultSet['circlename']."-".$resultSet['taluk']."-".$resultSet['statename'];
+     $res[$resultSet['id']]['value'] =  $resultSet['pincode'];
+    $res[$resultSet['id']]['label'] =  $resultSet['pincode'];
+    
+    }
+    if(!$res) {
+    	$res[0] = 'Not found!';
+    }
+    echo json_encode($res);
+}
+
+?>
